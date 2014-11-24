581f5116afc8467fdd0372b84042474abff49e79
Ryan Milvenan <ryanmilvenan@gmail.com>
2014-11-23 18:26:06 -0600

    Started glkit, not completely working yet

--- Milvenan Graphics Project/ViewController.h
+++ Milvenan Graphics Project/ViewController.h
@@ -7,8 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+@import GLKit;
 
-@interface ViewController : UIViewController
+@interface ViewController : GLKViewController
 
 
 @end
--- Milvenan Graphics Project/ViewController.m
+++ Milvenan Graphics Project/ViewController.m
@@ -12,16 +12,142 @@
 
 @end
 
-@implementation ViewController
+typedef struct {
+    float Position[3];
+    float Color[4];
+} SceneVertex;
+
+const SceneVertex vertices[] = {
+    {{-1, -1, 0}, {1, 1, 1, 1}},
+    {{1, -1, 0}, {1, 1, 1, 1}},
+    {{0, 0, 0}, {1, 1, 1, 1}},
+};
+
+const GLuint indices[] = {
+    0, 1, 2
+};
+
+
+@implementation ViewController {
+    GLuint _positionSlot;
+    GLuint _colorSlot;
+    GLuint _vertexBuffer;
+    GLuint _indexBuffer;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    GLKView *view = (GLKView *)self.view;
+    [self setupContext:view];
+    [self compileShaders];
+    [self setupVBOs];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)glkView:(GLKView *)view drawInRect:(CGRect)rect {
+    glClearColor(0, 104.0/255.0, 55.0/255.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    glEnableVertexAttribArray(_positionSlot);
+    glVertexAttribPointer(_positionSlot, 3, GL_FLOAT, GL_FALSE, sizeof(SceneVertex), (const GLvoid *) offsetof(SceneVertex, Position));
+    
+    glEnableVertexAttribArray(_colorSlot);
+    glVertexAttribPointer(_colorSlot, 4, GL_FLOAT, GL_FALSE, sizeof(SceneVertex), (const GLvoid *) offsetof(SceneVertex, Color));
+    
+    glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBuffer);
+    
+    glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(indices[0]), GL_UNSIGNED_BYTE, 0);
+}
+
+/************************************
+ *                                  *
+ *      Configuration Functions     *
+ *                                  *
+ ************************************/
+
+- (void)setupContext:(GLKView *)view {
+    EAGLContext* context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    view.context = context;
+}
+
+- (void)compileShaders {
+    GLuint vertexShader = [self compileShader:@"SimpleVertex"
+                                     ofType:GL_VERTEX_SHADER];
+    GLuint fragmentShader = [self compileShader:@"SimpleFragment"
+                                       ofType:GL_FRAGMENT_SHADER];
+    
+    GLuint programHandle = glCreateProgram();
+    glAttachShader(programHandle, vertexShader);
+    glAttachShader(programHandle, fragmentShader);
+    glLinkProgram(programHandle);
+    
+    GLint linkSuccess;
+    glGetProgramiv(programHandle, GL_LINK_STATUS, &linkSuccess);
+    if (linkSuccess == GL_FALSE) {
+        GLchar messages[256];
+        glGetProgramInfoLog(programHandle, sizeof(messages), 0, &messages[0]);
+        NSString *messageString = [NSString stringWithUTF8String:messages];
+        NSLog(@"%@", messageString);
+        exit(1);
+    }
+    
+    glUseProgram(programHandle);
+    
+    _positionSlot = glGetAttribLocation(programHandle, "Position");
+    _colorSlot = glGetAttribLocation(programHandle, "SourceColor");
+    glEnableVertexAttribArray(_positionSlot);
+    glEnableVertexAttribArray(_colorSlot);
+}
+
+- (void)setupVBOs {
+    
+    glGenBuffers(1, &_vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glGenBuffers(1, &_indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+}
+
+
+/************************************
+ *                                  *
+ *          Helper Functions        *
+ *                                  *
+ ************************************/
+
+- (GLuint)compileShader:(NSString*)shaderName ofType:(GLenum)shaderType {
+    
+    NSString* shaderPath = [[NSBundle mainBundle] pathForResource:shaderName
+                                                           ofType:@"glsl"];
+    NSError* error;
+    NSString* shaderString = [NSString stringWithContentsOfFile:shaderPath
+                                                       encoding:NSUTF8StringEncoding error:&error];
+    if (!shaderString) {
+        NSLog(@"Error loading shader: %@", error.localizedDescription);
+        exit(1);
+    }
+    
+    GLuint shader = glCreateShader(shaderType);
+    
+    const char * shaderStringUTF8 = [shaderString UTF8String];
+    int shaderStringLength = (int)[shaderString length];
+    glShaderSource(shader, 1, &shaderStringUTF8, &shaderStringLength);
+    
+    glCompileShader(shader);
+    
+    GLint compileSuccess;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &compileSuccess);
+    if (compileSuccess == GL_FALSE) {
+        GLchar messages[256];
+        glGetShaderInfoLog(shader, sizeof(messages), 0, &messages[0]);
+        NSString *messageString = [NSString stringWithUTF8String:messages];
+        NSLog(@"%@", messageString);
+        exit(1);
+    }
+    
+    return shader;
 }
 
 @end
